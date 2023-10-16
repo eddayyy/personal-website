2bac4eb45f7e8bb280a29d66454e2e7883af61e4
eddayyy <eduardonunez@csu.fullerton.edu>
2023-10-16 00:35:20 -0700

    Completed home section with links to github and linkedin!

--- src/js/main.js
+++ src/js/main.js
@@ -16,10 +16,45 @@ document.addEventListener('DOMContentLoaded', function () {
 
         a.href = item.link;
         a.textContent = item.name;
-
+        if (item.name === 'Home') {
+            a.classList.add('active-link');
+        }
         h4.appendChild(a);
         navbar.appendChild(h4);
     });
+});
+
+document.addEventListener('scroll', function () {
+    // Threshold for considering an element as 'in view'
+    const threshold = window.innerHeight / 2;
+
+    // Array of all sections
+    const sections = Array.from(document.querySelectorAll('section, header'));
+
+    // Find the section that's currently in view
+    let currentSection = null;
+    for (const section of sections) {
+        const box = section.getBoundingClientRect();
+        if (box.top <= threshold && box.top + box.height > threshold) {
+            currentSection = section;
+            break;
+        }
+    }
+
+    // If we found a section, highlight the corresponding navbar item
+    if (currentSection) {
+        // Remove previous active class
+        const previousActive = document.querySelector('.active-link');
+        if (previousActive) {
+            previousActive.classList.remove('active-link');
+        }
+
+        // Add active class to current item
+        const link = document.querySelector(`nav a[href="#${currentSection.id}"]`);
+        if (link) {
+            link.classList.add('active-link');
+        }
+    }
 });
 
 
